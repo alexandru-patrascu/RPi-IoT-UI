0240795946308ba02a438d99e738280cfb9046a5
alexandru-patrascu <alexandru.patrascu00@gmail.com>
2020-10-20 22:28:33 +0300

    mongodb setup

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+const db = require('./db');
+
 // Generate express instance
 const app = express();
 const port = 8000;
@@ -10,6 +12,9 @@ const port = 8000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(bodyParser.json());
+
+// Connect to MongoDB
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.listen(port, async () =>
   console.log(`\n==========\nServer running on port ${port}\n==========`)
