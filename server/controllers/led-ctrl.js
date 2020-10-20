813c7c8a46e5428de2401dcf43f6c6b3ae6a7f6d
alexandru-patrascu <alexandru.patrascu00@gmail.com>
2020-10-20 22:56:54 +0300

    updateOne led controller

--- server/controllers/led-ctrl.js
+++ server/controllers/led-ctrl.js
@@ -20,7 +20,41 @@ const getOne = async (req, res) => {
 
 const createOne = (req, res) => {};
 
-const updateOne = async (req, res) => {};
+const updateOne = async (req, res) => {
+  const { body } = req;
+
+  if (!body)
+    return res.status(400).json({
+      success: false,
+      error: 'You must provide the body in order to perform the update',
+    });
+
+  Led.findOne({ _id: req.params._id }, (err, led) => {
+    if (err)
+      return res.status(404).json({
+        err,
+        message: 'Led not found!',
+      });
+
+    led.name = body.name;
+    led.status = body.status;
+    led.gpioPin = body.gpioPin;
+    led
+      .save()
+      .then(() =>
+        res.status(200).json({
+          success: true,
+          _id: led._id,
+        })
+      )
+      .catch((error) =>
+        res.status(404).json({
+          success: false,
+          error,
+        })
+      );
+  });
+};
 
 const deleteOne = async (req, res) => {
   await Led.findOneAndDelete({ _id: req.params._id }, (error, led) => {
