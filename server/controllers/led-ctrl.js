fc2184b9bdae55343a855f0cd62fe86a49b9a120
alexandru-patrascu <alexandru.patrascu00@gmail.com>
2020-10-20 23:03:38 +0300

    small changes

--- server/controllers/led-ctrl.js
+++ server/controllers/led-ctrl.js
@@ -1,49 +1,46 @@
 const axios = require('axios');
 const Led = require('../models/led-model');
 
-const getAll = async (req, res) => {
+const getAll = async (req, res) =>
   await Led.find({}, (error, leds) => {
-    if (err || !!leds.length)
+    if (err || !leds.length)
       return res.status(404).json({ success: false, error });
 
     return res.status(200).json({ success: true, data: leds });
   }).catch((err) => console.error(err));
-};
 
-const getOne = async (req, res) => {
+const getOne = async (req, res) =>
   await Led.findOne({ _id: req.params._id }, (error, led) => {
     if (err || !led) return res.status(404).json({ success: false, error });
 
     return res.status(200).json({ success: true, data: led });
   }).catch((err) => console.error(err));
-};
 
 const createOne = (req, res) => {
   const { body } = req;
 
-  if (!body) {
+  if (!body)
     return res.status(400).json({
       success: false,
       error: 'You must provide a body in order to create the led entity',
     });
-  }
 
   const led = new Led(body);
 
   led
     .save()
-    .then(() => {
-      return res.status(201).json({
+    .then(() =>
+      res.status(201).json({
         success: true,
         _id: led._id,
-      });
-    })
-    .catch((error) => {
-      return res.status(400).json({
+      })
+    )
+    .catch((error) =>
+      res.status(400).json({
         success: false,
         error,
-      });
-    });
+      })
+    );
 };
 
 const updateOne = async (req, res) => {
@@ -56,7 +53,7 @@ const updateOne = async (req, res) => {
     });
 
   Led.findOne({ _id: req.params._id }, (err, led) => {
-    if (err)
+    if (err || !led)
       return res.status(404).json({
         err,
         message: 'Led not found!',
