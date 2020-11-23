4363e7f43cb555c7e6d089ce986795bd63de258f
alexandru-patrascu <alexandru.patrascu00@gmail.com>
2020-11-23 22:48:14 +0200

    handle delete led

--- client/src/components/leds/DeleteLed.jsx
+++ client/src/components/leds/DeleteLed.jsx
@@ -1,21 +1,35 @@
 import { DeleteOutlined } from '@ant-design/icons';
 import { Button, message, Popconfirm } from 'antd';
-import React from 'react';
+import React, { useContext } from 'react';
+import { deleteLed } from '../../handlers/ledHandlers';
+import { LedsContext } from '../../helpers';
 
 // import { deleteLed } from "../requests"
 
-const confirm = (_id, fullName) => {
-  // deleteLed(_id)
-  message.success(`Successfully deleted ${fullName}`);
-};
-
 const DeleteLed = (props) => {
   const { _id, name } = props;
+  const { leds, setLeds } = useContext(LedsContext);
+
+  const handleConfirm = async () => {
+    const response = await deleteLed(_id);
+
+    if (response === 200) {
+      // Update UI
+      const filteredLeds = leds.filter((led) => led._id !== _id);
+      setLeds(filteredLeds);
+
+      // Send Notification
+      message.success(`Successfully deleted ${name}!`);
+    } else
+      message.error(
+        `An error occurred while removing ${name} from DB. Please try again later!`
+      );
+  };
 
   return (
     <Popconfirm
       title={`Are you sure you want to delete ${name}?`}
-      onConfirm={() => confirm(_id, name)}
+      onConfirm={handleConfirm}
       okText="Yes"
     >
       <Button ghost size="small" type="primary" className="delete-button">
