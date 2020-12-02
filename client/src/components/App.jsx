046ffe8e5804b4c5137cc979a534181f76c04f89
alexandru-patrascu <alexandru.patrascu00@gmail.com>
2020-12-02 09:32:40 +0200

    rename app

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,8 +1,8 @@
 import { Layout } from 'antd';
 import React, { useCallback, useEffect, useState } from 'react';
 import { Route, Switch } from 'react-router-dom';
-import { getLeds } from '../handlers/ledHandlers';
-import { LedsContext } from '../helpers';
+import { getLights } from '../handlers';
+import { LightsContext } from '../helpers';
 import LoadingIndicator from './LoadingIndicator';
 import { Bedroom, BedroomLights, Dashboard, Home, NotFound } from './pages';
 import SideBar from './SideBar';
@@ -10,16 +10,16 @@ import SideBar from './SideBar';
 const { Header, Content, Footer } = Layout;
 
 const App = () => {
-  const [leds, setLeds] = useState([]);
-  const [currentLed, setCurrentLed] = useState(null);
-  const [showAddLedModal, setShowAddLedModal] = useState(false);
-  const [showUpdateLedModal, setShowUpdateLedModal] = useState(false);
+  const [lights, setLights] = useState([]);
+  const [currentLight, setCurrentLight] = useState(null);
+  const [showAddLightModal, setShowAddLightModal] = useState(false);
+  const [showUpdateLightModal, setShowUpdateLightModal] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
 
-  const fetchLeds = useCallback(async () => {
+  const fetchLights = useCallback(async () => {
     try {
-      const response = await getLeds;
-      setLeds(response);
+      const response = await getLights;
+      setLights(response);
     } catch (err) {
       console.error('An error occurred while fetching leds', err);
     } finally {
@@ -28,7 +28,7 @@ const App = () => {
   }, []);
 
   useEffect(() => {
-    fetchLeds();
+    fetchLights();
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
@@ -42,16 +42,16 @@ const App = () => {
         <Header>Header Content</Header>
 
         <Content className="content">
-          <LedsContext.Provider
+          <LightsContext.Provider
             value={{
-              leds,
-              setLeds,
-              currentLed,
-              setCurrentLed,
-              showAddLedModal,
-              setShowAddLedModal,
-              showUpdateLedModal,
-              setShowUpdateLedModal,
+              lights,
+              setLights,
+              currentLight,
+              setCurrentLight,
+              showAddLightModal,
+              setShowAddLightModal,
+              showUpdateLightModal,
+              setShowUpdateLightModal,
             }}
           >
             <Switch>
@@ -61,7 +61,7 @@ const App = () => {
               <Route path="/bedroom/lights" component={BedroomLights} />
               <Route component={NotFound} />
             </Switch>
-          </LedsContext.Provider>
+          </LightsContext.Provider>
         </Content>
 
         <Footer className="footer">
